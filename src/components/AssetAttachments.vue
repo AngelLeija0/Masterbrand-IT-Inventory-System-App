861ee19d848c6520f513dde7e1d6d9c512164fd2
Angel Gael Leija Limones <Angel.Leija@masterbrand.com>
2023-11-01 07:43:06 -0500

    Change in method downloadAttachment

--- src/components/AssetAttachments.vue
+++ src/components/AssetAttachments.vue
@@ -7,7 +7,11 @@
       <div class="row flex justify-between">
         <div class="col-lg-3 col-md-4 col-sm-6 col-12 q-pa-md" v-for="(attachment, i) in allAttachments" :key="i + 1">
           <q-card class="my-card text-center">
-            <q-card-section style="height: 200px; display: grid; align-items: center;">
+            <q-card-actions align="right" class="q-pt-sm q-pb-none">
+              <q-btn icon="download" color="black" flat round class="q-py-none" size="0.75rem"
+                @click="downloadAttachment(attachment)" />
+            </q-card-actions>
+            <q-card-section class="q-py-none" style="height: 200px; display: grid; align-items: center;">
               <div v-if="attachment.type === 'image'">
                 <q-img :src="attachment.url" style="width: 90%; height: 200px;" />
               </div>
@@ -18,8 +22,11 @@
                 <div class="text-red full-width">Error al mostrar el contenido</div>
               </div>
             </q-card-section>
-            <q-card-section>
-              <div style="font-size: 0.75rem;">{{ attachment.label }}</div>
+            <q-card-section style="height: 85px; display: grid; align-items: center;">
+              <div class="q-pt-sm" style="font-size: 0.85rem;">{{ attachment.label }}</div>
+              <div v-if="defaultImage === attachment.label" class="text-grey-14 q-pt-xs" style="font-size: 0.75rem;">
+                Imagen predeterminada
+              </div>
             </q-card-section>
           </q-card>
         </div>
@@ -62,7 +69,7 @@ export default defineComponent({
 
     const idAsset = ref(router.params.id)
 
-    console.log(props.asset)
+    const defaultImage = ref(defineFileName(props.asset.images.default_image))
 
     const allAttachments = ref([])
 
@@ -116,10 +123,23 @@ export default defineComponent({
       attachmentsURL,
       idAsset,
       allAttachments,
+      defaultImage,
     }
   },
   methods: {
+    downloadAttachment(attachment) {
+      fetch(url)
+        .then(res => res.blob())
+        .then(blob => {
+          let imgURL = URL.createObjectURL(blob)
 
+          let a = document.createElement('a');
+          a.href = imgURL;
+          a.download = 'imagen.jpg';
+          a.click();  
+        })
+        .catch(e => console.error(e));
+    }
   }
 });
 </script>
